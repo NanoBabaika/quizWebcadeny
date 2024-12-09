9dac923f88eb05427f647df48616516514543a37
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2024-12-09 22:24:27 +0300

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,5 @@
 console.log('Lets Code! {^_^}');
-console.log('Движение вперед. Несущ. карточка 3:20');
+console.log('Урок 7. Прогресс бар. Часть 1');
 
 const cards = document.querySelectorAll('.plate');
  
@@ -13,20 +13,27 @@ console.log('currentCard', currentCard);
 // перемещение по карточкам
 window.addEventListener ('click', function (event) {
      
+    // Движение вперед
     if(event.target.closest('[data-nav = "next"]')) {
         console.log('next page');
 
-        // if(currentCard === cards.length -1) {
-        //     return;
-        // }
+        const result = checkOnAnAnser(cards[currentCard]);
+        const answersWrapper = cards[currentCard].querySelector('[data-answers]');
+
+        if(result) {
+            answersWrapper.classList.remove('required');
+            cards[currentCard].classList.remove('visible');
+            // увеличиваем индекс массива с карточками
+            currentCard = currentCard + 1;
+            // показываем карточку с обновленным индексом
+            cards[currentCard].classList.add('visible');    
+        } else {
+            answersWrapper.classList.add('required');
+            alert('Выберите ответ что бы продолжить');
+        } 
 
         // скрываем текущую карточку
-        cards[currentCard].classList.remove('visible');
-        // увеличиваем индекс массива с карточками
-        currentCard = currentCard + 1;
-        // показываем карточку с обновленным индексом
-        cards[currentCard].classList.add('visible');
-        console.log('currentCard', currentCard);
+         console.log('currentCard', currentCard);
     }
 
     if(event.target.closest('[data-nav = "prev"]')) {
@@ -49,9 +56,24 @@ window.addEventListener ('click', function (event) {
 
 // проверка полей на заполенность
 
-function checkOnAnAnser() {
-    // проверка радио кнопки
-    
+function checkOnAnAnser(card) {
+     // проверка радио кнопки
+    const radioBtns = card.querySelectorAll('input[type="radio"]');
+
+    if(radioBtns.length > 0) {
+        for (let radio of radioBtns) if(radio.checked) return true;
+    }
+
+    // проверка на чекбоксы
+    const checkBoxes = card.querySelectorAll('input[type="checkbox"]');
+    console.log(checkBoxes);
+    if(checkBoxes.length > 0) {
+        for(let checkBox of checkBoxes) {
+            if(checkBox.checked) return true;
+        }
+    }
 
 
-}
+}  
+
+
